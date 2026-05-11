662a0332d08f0703f5ad902df6c0b292007b7e58
Al-ameen Abiola <alameenabiola3@gmail.com>
2026-05-11 12:08:17 +0100

    pivot tables

--- summary_statistics.py
+++ summary_statistics.py
@@ -6,6 +6,17 @@ sales = pd.read_csv("sales_subset.csv", index_col=0)
 sales_1_1 = sales.copy()
 
 
+
+# Force pandas to show all rows and columns in the CLI
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+# Optional: Prevent text from wrapping to a new line if the table is wide
+pd.set_option('display.width', 1000)
+
+
+
+
+
 # # Print the head of the sales DataFrame
 # print(sales.head())
 
@@ -138,14 +149,47 @@ print(sales_by_type_is_holiday)
 Multiple grouped summaries with .agg()
 '''
 
-# For each store type, aggregate weekly_sales: get min, max, mean, and median
-sales_stats = sales.groupby("type")["weekly_sales"].agg([min, max, "mean", "median"])
+# # For each store type, aggregate weekly_sales: get min, max, mean, and median
+# sales_stats = sales.groupby("type")["weekly_sales"].agg([min, max, "mean", "median"])
 
-# Print sales_stats
-print(sales_stats)
+# # Print sales_stats
+# print(sales_stats)
 
-# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
-unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"]].agg([min, max, "mean", "median"])
+# # For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+# unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"]].agg([min, max, "mean", "median"])
 
-# Print unemp_fuel_stats
-print(unemp_fuel_stats)
+# # Print unemp_fuel_stats
+# print(unemp_fuel_stats)
+
+
+'''
+Pivot table - as an alternative to groupby for calculating grouped summary statistics, especially when you want to calculate multiple stats or pivot on multiple variables
+Pivoting on one variable with .pivot_table()
+'''
+
+# # Pivot for mean weekly_sales for each store type
+# mean_sales_by_type = sales.pivot_table(values="weekly_sales", index="type")
+
+# # Print mean_sales_by_type
+# print(mean_sales_by_type)
+
+# # Pivot for mean and median weekly_sales for each store type
+# mean_med_sales_by_type = sales.pivot_table(values = "weekly_sales", index= "type", aggfunc = ["mean", "median"])
+
+# # Print mean_med_sales_by_type
+# print(mean_med_sales_by_type)
+
+# # Pivot for mean weekly_sales by store type and holiday 
+# mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index = "type", columns="is_holiday")
+
+# # Print mean_sales_by_type_holiday
+# print(mean_sales_by_type_holiday)
+
+'''
+fill in missing values and values with pivot tables
+'''
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value = 0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins = True))
