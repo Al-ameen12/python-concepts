5ebb2c9cc607c3b0179987d2aeafe9dbb421d770
Al-ameen Abiola <alameenabiola3@gmail.com>
2026-05-14 15:06:48 +0100

    one to many merge

--- data_merging.py
+++ data_merging.py
@@ -3,8 +3,16 @@ import pandas as pd
 taxi_owners = pd.read_csv('taxi_owners.csv')
 taxi_veh = pd.read_csv('taxi_vehicles.csv')
 
+
 wards = pd.read_csv('Chicago_wards.csv')
 census = pd.read_csv('Chicago_census.csv')
+
+wards_altered = wards.copy()
+census_altered = census.copy()
+
+
+licenses = pd.read_csv('licenses.csv')
+biz_owners = pd.read_csv('business_owners.csv')
 
 
 '''
@@ -37,27 +45,43 @@ Chicago wards and census data
 # print(wards['ward'].value_counts())
 
 # Merge the wards and census tables on the ward column
-wards_census = wards.merge(census, on='ward')
+# wards_census = wards.merge(census, on='ward')
 
-# Print the shape of wards_census
-print('wards_census table shape:', wards_census.shape)
-
-
-# Print the first few rows of the wards_altered table to view the change 
-print(wards_altered[['ward']].head())
-
-# Merge the wards_altered and census tables on the ward column
-wards_altered_census = wards_altered.merge(census, on='ward')
-
-# Print the shape of wards_altered_census
-print('wards_altered_census table shape:', wards_altered_census.shape)
+# # Print the shape of wards_census
+# print('wards_census table shape:', wards_census.shape)
 
 
-# Print the first few rows of the census_altered table to view the change 
-print(census_altered[['ward']].head())
+# # Print the first few rows of the wards_altered table to view the change 
+# print(wards_altered[['ward']].head())
 
-# Merge the wards and census_altered tables on the ward column
-wards_census_altered = wards.merge(census_altered, on = 'ward')
+# # Merge the wards_altered and census tables on the ward column
+# wards_altered_census = wards_altered.merge(census, on='ward')
 
-# Print the shape of wards_census_altered
-print('wards_census_altered table shape:', wards_census_altered.shape)
+# # Print the shape of wards_altered_census
+# print('wards_altered_census table shape:', wards_altered_census.shape)
+
+
+# # Print the first few rows of the census_altered table to view the change 
+# print(census_altered[['ward']].head())
+
+# # Merge the wards and census_altered tables on the ward column
+# wards_census_altered = wards.merge(census_altered, on = 'ward')
+
+# # Print the shape of wards_census_altered
+# print('wards_census_altered table shape:', wards_census_altered.shape)
+
+
+
+# Merge the licenses and biz_owners table on account
+licenses_owners = licenses.merge(biz_owners, on = 'account')
+
+# Group the results by title then count the number of accounts
+counted_df = licenses_owners.groupby('title').agg({'account':'count'})
+# an alternative way to do the above is: 
+# counted_df = licenses_owners.groupby('title')['account'].count()
+
+# Sort the counted_df in descending order
+sorted_df = counted_df.sort_values('account', ascending = False)
+
+# Use .head() method to print the first few rows of sorted_df
+print(sorted_df.head())
