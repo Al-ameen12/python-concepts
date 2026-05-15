be9a9c24f2c7625f939407f8a0dea9aebbe21b8f
Al-ameen Abiola <alameenabiola3@gmail.com>
2026-05-15 21:11:41 +0100

    use other joins-left, right, outer

--- data_merging.py
+++ data_merging.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 taxi_owners = pd.read_csv('taxi_owners.csv')
 taxi_veh = pd.read_csv('taxi_vehicles.csv')
@@ -21,6 +22,31 @@ cal = pd.read_csv('cta_calendar.csv')
 ridership = pd.read_csv('cta_ridership.csv')
 stations = pd.read_csv('stations.csv')
 
+
+movies = pd.read_csv('movies.csv')
+financials = pd.read_csv('financials.csv')
+movies_genres = pd.read_csv('movie_to_genres.csv', index_col=0)
+pop_movies = pd.read_csv('pop_movies.csv', index_col=0)
+
+'''
+create action and scifi movies tables by filtering the movies_genres 
+table for the genres 'Action' and 'Science Fiction', respectively.
+
+'''
+# print(movies_genres.head())
+scifi_movies = movies_genres[movies_genres['genre'] == 'Science Fiction'].reset_index(drop=True)
+scifi_movies.to_csv('scifi_movies.csv')
+
+action_movies = movies_genres[movies_genres['genre'] == 'Action'].reset_index(drop=True)
+action_movies.to_csv('action_movies.csv')
+
+'''
+Different types of joins
+inner join: returns only the rows that have matching values in both tables.
+left join: returns all the rows from the left table, and the matched rows from the right
+outer join: returns all the rows from both tables, with NaN in places where the join keys did not match.
+right join: returns all the rows from the right table, and the matched rows from the left.
+'''
 
 '''
 Inner Joins of tables.
@@ -123,19 +149,19 @@ Merging Multiple DataFrames
 
 
 # Merge land_use and census and merge result with licenses including suffixes
-land_cen_lic = land_use.merge(census, on='ward') \
-                    .merge(licenses, on='ward', suffixes=('_cen','_lic'))
+# land_cen_lic = land_use.merge(census, on='ward') \
+#                     .merge(licenses, on='ward', suffixes=('_cen','_lic'))
 
-# Group by ward, pop_2010, and vacant, then count the # of accounts
-pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'], 
-                                   as_index=False).agg({'account':'count'})
+# # Group by ward, pop_2010, and vacant, then count the # of accounts
+# pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'], 
+#                                    as_index=False).agg({'account':'count'})
 
-# Sort pop_vac_lic and print the results
-sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account', 'pop_2010'], 
-                                             ascending= [False, True, False])
+# # Sort pop_vac_lic and print the results
+# sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account', 'pop_2010'], 
+#                                              ascending= [False, True, False])
 
-# Print the top few rows of sorted_pop_vac_lic
-print(sorted_pop_vac_lic.head())
+# # Print the top few rows of sorted_pop_vac_lic
+# print(sorted_pop_vac_lic.head())
 
 
 
@@ -147,3 +173,55 @@ then you do not want to lose any of your original data.
 A left join will do that by returning all of the rows of your left table, 
 while using an inner join may result in lost data if it does not exist in both tables.
 '''
+
+# Merge the movies table with the financials table with a left join
+# movies_financials = movies.merge(financials, on='id', how='left')
+
+# # Count the number of rows in the budget column that are missing
+# number_of_missing_fin = movies_financials['budget'].isna().sum()
+
+# # Print the number of movies missing financials
+# print(number_of_missing_fin)
+
+
+# # Merge action_movies to the scifi_movies with right join
+# action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+#                                    suffixes=('_act','_sci'))
+
+# # From action_scifi, select only the rows where the genre_act column is null
+# scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+# # Merge the movies and scifi_only tables with an inner join
+# movies_and_scifi_only = movies.merge(scifi_only, left_on = 'id', right_on = 'movie_id')
+
+# # Print the first few rows and shape of movies_and_scifi_only
+# print(movies_and_scifi_only.head())
+# print(movies_and_scifi_only.shape)
+
+
+# # Use right join to merge the movie_to_genres and pop_movies tables
+# genres_movies = movies_genres.merge(pop_movies, how='right', 
+#                                       right_on = 'id', 
+#                                       left_on = 'movie_id')
+
+# # Count the number of genres
+# genre_count = genres_movies.groupby('genre').agg({'id':'count'})
+
+# # Plot a bar chart of the genre_count
+# genre_count.plot(kind='bar')
+# plt.show()
+
+
+
+# Merge iron_1_actors to iron_2_actors on id with outer join using suffixes
+iron_1_and_2 = iron_1_actors.merge(iron_2_actors,
+                                     on = 'id',
+                                     how = 'outer',
+                                     suffixes=('_1', '_2'))
+
+# Create an index that returns true if name_1 or name_2 are null
+m = ((iron_1_and_2['name_1'].isnull()) | 
+     (iron_1_and_2['name_2'].isnull()))
+
+# Print the first few rows of iron_1_and_2
+print(iron_1_and_2[m].head())
